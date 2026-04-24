bc6de01884db219d6db83ea81fd17e8a4b266c1a
JuwelRana34 <juwelrana3426@gmail.com>
2026-04-24 16:41:12 +0600

    Refactor conditional rendering for user avatar in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,15 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-bold underline">Hello {session?.data?.user?.name || "World"}!</h1>
       <Link className="my-2 w-fit bg-gray-500 text-white rounded " href="/register">Go to Register</Link>
-      <Image
+    {!session?"":<> 
+    <Image
       className="rounded-full"
        src={session?.data?.user?.image || "/default-avatar.png"}
         alt="User Avatar"
         width={100}
         height={100}
       />
+    </>}  
       <p> your email: {session?.data?.user?.email}</p>
       <Link className=" bg-gray-500 text-white rounded " href="/signUp">Go to Sign In</Link>
 
