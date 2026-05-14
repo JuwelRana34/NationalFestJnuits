3ea2126d19b40a1eaf602bf47ef5fd4161e6a3dd
JuwelRana34 <juwelrana3426@gmail.com>
2026-05-14 14:46:51 +0600

    feat: Update DashboardWrapner to include cookie handling in API requests

--- features/dashboard/components/DashboardWrapner.tsx
+++ features/dashboard/components/DashboardWrapner.tsx
@@ -1,10 +1,21 @@
 import { honoFetch } from "@/lib/hono-client";
 import { DashboardResponse } from "../Types";
 import Dashboard from "./DashboardOverview";
+import { cookies } from "next/headers";
 
 export default async function DashboardWrapner() {
+ const cookieStore = await cookies(); // ← কুকি নাও
+ const allCookies = cookieStore
+   .getAll()
+   .map((c) => `${c.name}=${c.value}`)
+   .join("; ");
 
-     const { data } = await honoFetch<DashboardResponse>("/api/users/dashboard");
+     const { data } = await honoFetch<DashboardResponse>(
+       "/api/users/dashboard",
+       {
+         headers: { allCookies },
+       },
+     );
     //  console.log("Dashboard data:", data);
     return <Dashboard DashboardData={data} />;
 }
