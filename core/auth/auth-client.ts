957beba71e336c3771b482ec7d6afe481e7b94d8
JuwelRana34 <juwelrana3426@gmail.com>
2026-05-15 09:30:10 +0600

    fix: Update API URLs in auth client and configuration files

--- core/auth/auth-client.ts
+++ core/auth/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8787"
-    : "https://jnu-it-fest.rk370613.workers.dev";
+    : "https://festapi.jnuits.org.bd";
 
 
 export const authClient = createAuthClient({
