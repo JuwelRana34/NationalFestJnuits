59d55b487708d606851d1c502873f27a6fe3a143
JuwelRana34 <juwelrana3426@gmail.com>
2026-06-04 22:35:31 +0600

    fix: ensure requireAuth is set to false in EventDataFetch for public access

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -24,6 +24,7 @@ export default function EventPage() {
 async function EventDataFetch() {
   const { response } = await honoFetch<EventsResponse>("/api/events",{
     next: { revalidate: 86000, tags: [`events`] },
+    requireAuth: false,
   })
   
   return (
